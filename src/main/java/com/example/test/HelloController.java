8e7ba90d390f75d538185de4e45e71ca1d9b5c4f
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-01-19 01:44:23 +0900

    keokbs/servlet_211015#19 lesson01, lesson02 예제

--- src/main/java/com/example/test/HelloController.java
+++ src/main/java/com/example/test/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.test;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 	
+	@Autowired
+	private TestDAO testDAO;
+	
 	@ResponseBody
 	@RequestMapping("/test1")
 	public String test1() {
 		return "hello world!!!";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/db_test")
+	public List<Map<String, Object>> dbTest() {
+		return testDAO.selectUser();
 	}
 }
