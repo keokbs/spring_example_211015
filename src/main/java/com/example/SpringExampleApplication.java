ed0e60a2a0f5cb5625dbb8ae1d58a12ce7af50e0
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-01-25 01:46:05 +0900

    keokbs/spring_example_211015#1 lesson03 CRUD 예제

--- src/main/java/com/example/SpringExampleApplication.java
+++ src/main/java/com/example/SpringExampleApplication.java
@@ -1,15 +1,16 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) DB 연동을 안한다는 어노테이션
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SpringExampleApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
-		
 	}
-	
+
 }
