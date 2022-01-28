ec9e6302e095a861578380431156ee1c626349d6
DESKTOP-7KO2LJC\KHJ <keokbs@gmail.com>
2022-01-28 17:54:39 +0900

    no message

--- src/main/java/com/example/lesson05/Lesson05Controller.java
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -58,10 +58,13 @@ public class Lesson05Controller {
 		return "lesson05/ex03";
 	}
 	
+<<<<<<< HEAD
 	// http://localhost:8080/lesson05/ex04
 		@RequestMapping("/lesson05/ex04")
 		public String ex04(){
 			return "lesson05/ex04";
 		}
 	
+=======
+>>>>>>> origin/develop
 }
